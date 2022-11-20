77bb8645a270b097a0b9e4985eed91661aca7643
Samyar Feroni <s.feroni@avakatan.ir>
2022-11-20 16:18:43 +0330

    The p1 of js

--- p1.js
+++ p1.js
@@ -1,1 +1,12 @@
-console.log("HI")
+function addArray(array1, array2) {
+    let list = []
+        for(let i = 0;i<array1.length;i++){
+            array1.reverse()
+            console.log(i)
+            list.push((`${i+1}${array1[i]} choice is ${array2[i]}`))
+    }
+    return list
+};
+let o = ['rd','th','nd','st'];
+let color = ['Blue','Green','Red',"Black","Yellow","Orange", "White","Purple","Violet","Indigo', 'Gray","Pink"];
+console.log(addArray(o,color));
