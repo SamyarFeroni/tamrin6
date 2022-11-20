0dcc8d0dcd47c22596991cf2af37132055a37da9
Samyar Feroni <s.feroni@avakatan.ir>
2022-11-20 18:57:02 +0330

    array and Obj

---
+++ p5.js
@@ -0,0 +1,36 @@
+let personData = [
+    {
+        uid: 1,
+        firstName: 'samyar',
+        lastName: 'feroni',
+    },
+    {
+        uid: 2,
+        firstName: 'mahyar',
+        lastName: 'feroni'
+    },
+];
+let additionalPersonData = [
+    {
+        uid: 1,
+        postion: "Back-End Developer",
+        city: "Tehran"
+    },
+    {
+        uid: 2,
+        postion: "Front-End Developer",
+        city: "Qeshm"
+    },
+];
+function mixByUid(array1, array2) {
+    let list = []
+    array1.forEach(x => {
+        array2.forEach(y => {
+            if (x.uid === y.uid) {
+                list.push({ ...x, ...y })
+            }
+        })
+    });
+    return list
+};
+console.log(mixByUid(personData, additionalPersonData));
